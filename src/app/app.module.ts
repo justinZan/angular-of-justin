44037eec6c030f371dab4f6575bde75d250b9e2c
dingzan <1105503089@qq.com>
2021-03-23 14:41:54 +0800

    增加amcharts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppComponent } from './app.component';
 import { StorageService } from './service/storage.service';
 import { ChartComponent } from './components/chart/chart.component';
 
+
 /* @NgModule装饰器，@NgModule接受一个元数据对象，告诉Angular 如何编译和启动应用*/
 @NgModule({
   declarations: [ // 配置当前项目运行的组件
@@ -25,7 +26,7 @@ import { ChartComponent } from './components/chart/chart.component';
   imports: [ // 配置当前模块依赖的其他模块
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [StorageService], // 配置项目所需要的服务
   bootstrap: [AppComponent] // 指定应用的主视图（称为根组件），通过引导根AppModule来启动应用，这里一般写的是根组件
--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// am4charts
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+    const chart: any = am4core.create('chartdiv', am4charts.PieChart);
+
+    chart.data = [{
+      country: 'Lithuania',
+      litres: 501.9
+    }, {
+      country: 'Czech Republic',
+      litres: 301.9
+    }, {
+      country: 'Ireland',
+      litres: 201.1
+    }, {
+      country: 'Germany',
+      litres: 165.8
+    }, {
+      country: 'Australia',
+      litres: 139.9
+    }, {
+      country: 'Austria',
+      litres: 128.3
+    }, {
+      country: 'UK',
+      litres: 99
+    }, {
+      country: 'Belgium',
+      litres: 60
+    }, {
+      country: 'The Netherlands',
+      litres: 50
+    }];
+
+    const pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = 'litres';
+    pieSeries.dataFields.category = 'country';
   }
 
 }
